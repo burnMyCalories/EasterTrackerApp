b5a5f83da2efea7f7176737546daec4de478e580
sxn2012 <xinnan.shen@outlook.com.au>
2020-10-17 11:58:51 +0800

    modify ui

--- app/src/main/java/com/materialstudies/reply/ui/home/Mailbox.kt
+++
@@ -1,8 +0,0 @@
-package com.materialstudies.reply.ui.home
-
-/**
- * An enumeration of mailboxes into which emails can be placed.
- */
-enum class Mailbox {
-  INBOX, STARRED, SENT, TRASH, SPAM, DRAFTS
-}
