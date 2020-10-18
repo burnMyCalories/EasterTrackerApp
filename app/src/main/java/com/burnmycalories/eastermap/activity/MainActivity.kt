29e2c0cafa75acac34e92f78e198f4cac6d22b47
sxn2012 <xinnan.shen@outlook.com.au>
2020-10-18 13:38:20 +0800

    reset permission

--- app/src/main/java/com/burnmycalories/eastermap/activity/MainActivity.kt
+++ app/src/main/java/com/burnmycalories/eastermap/activity/MainActivity.kt
@@ -32,28 +32,30 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.burnmycalories.eastermap.R
 
+
 class MainActivity : AppCompatActivity()
 {
 
 
-
-
+    val url = "file:////android_asset/homeMap.html"
+    lateinit var webView:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
-        val url = "file:////android_asset/homeMap.html"
-        val wview = findViewById<View>(R.id.webView) as WebView
-        wview.settings.javaScriptEnabled = true
-        wview.settings.javaScriptCanOpenWindowsAutomatically = true
-        wview.settings.setGeolocationEnabled(true)
-        wview.settings.databaseEnabled = true
-        wview.settings.allowFileAccess = true
-        wview.settings.allowFileAccessFromFileURLs = true
-        wview.settings.allowUniversalAccessFromFileURLs = true
-        wview.settings.domStorageEnabled = true
-        wview.settings.useWideViewPort = true
-        wview.settings.builtInZoomControls = true
-        wview.webChromeClient = object : WebChromeClient() {
+        val wview: WebView
+        wview = findViewById<View>(R.id.webView) as WebView
+        webView=wview
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.setGeolocationEnabled(true)
+        webView.settings.databaseEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.useWideViewPort = true
+        webView.settings.builtInZoomControls = true
+        webView.webChromeClient = object : WebChromeClient() {
             override fun onGeolocationPermissionsShowPrompt(origin: String, callback: GeolocationPermissions.Callback) {
                 callback.invoke(origin, true, false)
                 super.onGeolocationPermissionsShowPrompt(origin, callback)
@@ -68,8 +70,14 @@ class MainActivity : AppCompatActivity()
                 ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
             } else {
                 println("Success")
+                startWebView(webView)
             }
         }
+
+
+    }
+
+    fun startWebView(wview: WebView){
         wview.loadUrl(url)
         wview.setWebViewClient(object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
@@ -84,17 +92,42 @@ class MainActivity : AppCompatActivity()
 //        wview.post {
 //            wview.loadUrl("javascript:clickJS2()")
 //        }
-
     }
-
 
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
         return super.onCreateView(name, context, attrs)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
 
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                                grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    startWebView(webView)
 
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                    System.exit(0)
+                }
+                return
+            }
 
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
 
 
 
