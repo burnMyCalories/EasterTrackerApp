f08e2b8f83742f66f52c8fdececc658cc847157f
sxn2012 <xinnan.shen@outlook.com.au>
2020-10-19 19:42:47 +0800

    demo backend system

---
+++ Easter-Tracker-Admin/Easter-Tracker-Admin/src/main/java/com/servlet/ErrorServlet.java
@@ -0,0 +1,31 @@
+package com.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+@WebServlet(name = "error", urlPatterns = "/error")
+public class ErrorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>404</h1><br/>");
+
+        ServletContext context = this.getServletContext();
+        RequestDispatcher requestDispatcher = context.getRequestDispatcher("/hello");//转发的请求路径
+        requestDispatcher.forward(req,resp);//请求转发
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
