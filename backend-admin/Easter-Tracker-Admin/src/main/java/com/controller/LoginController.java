bf6ed1ca2e2a7a58d2f662512c0c3be9b95bde85
sxn2012 <xinnan.shen@outlook.com.au>
2020-10-22 23:24:36 +0800

    test backend

--- backend-admin/Easter-Tracker-Admin/src/main/java/com/controller/LoginController.java
+++ backend-admin/Easter-Tracker-Admin/src/main/java/com/controller/LoginController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.util.Arrays;
 
@@ -16,15 +17,27 @@ public class LoginController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.setCharacterEncoding("utf-8");
-//        String username=req.getParameter("username");
-//        String password=req.getParameter("password");
+        req.setCharacterEncoding("utf-8");
+        String username=req.getParameter("userName");
+        String password=req.getParameter("passWord");
+        if(username.trim().equals("admin")&&password.trim().equals("admin")){
+            resp.setCharacterEncoding("utf-8");
+            PrintWriter out = resp.getWriter();
+            out.append("{success}");
+//            resp.sendRedirect("/test/success.jsp");
+//            req.setAttribute("userName",username);
+//            req.getRequestDispatcher("/success.jsp").forward(req,resp);
+        }
+        else{
+            req.getRequestDispatcher("/error.jsp").forward(req,resp);
+        }
 //        System.out.println(username);
 //        System.out.println(password);
 //        String[] hobbies = req.getParameterValues("hobbies");
 //        System.out.println(Arrays.toString(hobbies));
 ////        req.getRequestDispatcher("/success.jsp").forward(req,resp);
-        Connection connection = DBUtils.getConnection();
+//        Connection connection = DBUtils.getConnection();
+
     }
 
 
